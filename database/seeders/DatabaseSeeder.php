216f7552de17d35298b5ab4ea1fd38dd93164cbd
Meheady <hmmehedi55@gmail.com>
2023-08-31 09:14:42 +0600

    start working student registration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//     User::factory(10)->create();
+   User::factory(10)->create();
 //      Permission::factory(10)->create();
 //        Role::factory(10)->create();
 
