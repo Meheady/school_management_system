c4c09ab7d575d43ac85dacd2c14e3e129514200e
Meheady <hmmehedi55@gmail.com>
2023-10-17 09:43:52 +0600

    complete insert attendence

--- app/Http/Controllers/backend/employee/EmployeeAttendenceController.php
+++ app/Http/Controllers/backend/employee/EmployeeAttendenceController.php
@@ -15,7 +15,35 @@ class EmployeeAttendenceController extends Controller
             return $allAttendance;
         }
         catch (\Exception $e){
+            return  apiError($e->getMessage());
+        }
+    }
+    public function store(Request $request)
+    {
+        try {
 
+            $attendanceDate = $request->attendanceDate;
+            $countData = count($request->employeeStatus);
+            $employeeStatus = $request->employeeStatus;
+
+            if ($countData > 0){
+                $attendanceData = [];
+                foreach ($employeeStatus as $item){
+                    $record = [
+                        'employee_id'=>$item['employeeId'],
+                        'status'=>$item['status'],
+                        'date'=>$attendanceDate
+                    ];
+
+                    $attendanceData[] = $record;
+                }
+                EmployeeAttendence::storeAttendance($attendanceData);
+
+                return apiResponse('Attendance insert successfully');
+            }
+
+        } catch (\Exception $e) {
+            return  apiError($e->getMessage());
         }
     }
 }
--- app/Models/EmployeeAttendence.php
+++ app/Models/EmployeeAttendence.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class EmployeeAttendence extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+    protected $table = 'employee_attendances';
+    public static function storeAttendance($data)
+    {
+        EmployeeAttendence::insert($data);
+    }
 }
