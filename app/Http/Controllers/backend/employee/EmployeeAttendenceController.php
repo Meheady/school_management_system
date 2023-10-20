d5836a0e6e0763edf774bbbd04c06f56cf1405ac
Meheady <hmmehedi55@gmail.com>
2023-10-20 22:26:18 +0600

    Update EmployeeAttendenceController.php
    complete attendence part

--- app/Http/Controllers/backend/employee/EmployeeAttendenceController.php
+++ app/Http/Controllers/backend/employee/EmployeeAttendenceController.php
@@ -21,7 +21,8 @@ class EmployeeAttendenceController extends Controller
     public function show($date)
     {
         try {
-            $allAttendance = EmployeeAttendence::with('user')->where('date', $date)->get();
+            $getdate = date('Y-m-d',strtotime($date));
+            $allAttendance = EmployeeAttendence::with('user')->where('date', $getdate)->get();
             return apiResponse($allAttendance);
         }
         catch (\Exception $e){
@@ -50,6 +51,34 @@ class EmployeeAttendenceController extends Controller
                 EmployeeAttendence::storeAttendance($attendanceData);
 
                 return apiResponse('Attendance insert successfully');
+            }
+
+        } catch (\Exception $e) {
+            return  apiError($e->getMessage());
+        }
+    }
+    public function update(Request $request)
+    {
+        try {
+            $attendanceDate = date('Y-m-d', strtotime($request->attendanceDate));
+            EmployeeAttendence::where('date', $attendanceDate)->delete();
+            $countData = count($request->employeeStatus);
+            $employeeStatus = $request->employeeStatus;
+
+            if ($countData > 0){
+                $attendanceData = [];
+                foreach ($employeeStatus as $item){
+                    $record = [
+                        'employee_id'=>$item['employeeId'],
+                        'status'=>$item['status'],
+                        'date'=>$attendanceDate
+                    ];
+
+                    $attendanceData[] = $record;
+                }
+                EmployeeAttendence::storeAttendance($attendanceData);
+
+                return apiResponse('Attendance update successfully');
             }
 
         } catch (\Exception $e) {
